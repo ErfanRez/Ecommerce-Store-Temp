2e642c85dbaa8aa41959a56b3e3c8263022914aa
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2023-09-26 20:51:47 +0300

    Products card list added.

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,7 @@
 import GetBillboards from "@/actions/GetBillboards";
+import GetProducts from "@/actions/GetProducts";
 import Billboard from "@/components/Billboard";
+import ProductList from "@/components/ProductList";
 import Container from "@/components/ui/Container";
 import React from "react";
 
@@ -7,11 +9,15 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const billboard = await GetBillboards("548b8bee-2808-480b-9474-af94d49229a5");
+  const products = await GetProducts({ isFeatured: true });
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   );
