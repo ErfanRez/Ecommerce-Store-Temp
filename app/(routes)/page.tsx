5d76e7cb9af5f659b515b93b19b76342aa32a957
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2023-09-26 19:28:43 +0300

    Billboard section added to main page.

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,20 @@
+import GetBillboards from "@/actions/GetBillboards";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/Container";
 import React from "react";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await GetBillboards("548b8bee-2808-480b-9474-af94d49229a5");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
