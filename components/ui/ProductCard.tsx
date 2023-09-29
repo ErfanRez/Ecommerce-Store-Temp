13148a3af242278a5d5af03a5cea30c71f897786
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2023-09-29 03:55:25 +0300

    Preview modal component added.

--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -9,16 +9,23 @@ import { formatter } from "@/lib/utils";
 import IconButton from "@/components/ui/IconButton";
 import Currency from "@/components/ui/Currency";
 import { Product } from "@/types";
+import usePreview from "@/hooks/usePreview";
 
 interface ProductCard {
   data: Product;
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  const previewModal = usePreview();
   const router = useRouter();
 
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
+  };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+    previewModal.onOpen(data);
   };
 
   return (
@@ -37,7 +44,7 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
