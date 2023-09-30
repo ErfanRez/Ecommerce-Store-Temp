a8e18011e348e649e237c1ea44e9d3b8f6533ffb
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2023-10-01 02:25:56 +0300

    Final Changes.

--- actions/GetProducts.tsx
+++ actions/GetProducts.tsx
@@ -16,7 +16,7 @@ const GetProducts = async (query: Query): Promise<Product[]> => {
     query: {
       colorId: query.colorId,
       sizeId: query.sizeId,
-      category: query.categoryId,
+      categoryId: query.categoryId,
       isFeatured: query.isFeatured,
     },
   });
